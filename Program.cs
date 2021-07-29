a77c5dd18681f4370f2fbe4d72ea39c5ef1cffae
Milagro Scardigno <mdscardigno@gmail.com>
2021-07-28 23:14:45 -0400

    finished with explorer mode of AllCardsOnDeck

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+// using System.Linq;
 
 namespace AllCardsOnDeckCS
 {
@@ -6,7 +8,62 @@ namespace AllCardsOnDeckCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to All Cards on the Deck.");
+            // Requirements
+            // create a deck variable as a list and initialize as empty
+            var deck = new List<string>();
+            // create a card variable as a list and initialize as empty
+            var card = new List<string>();
+            // All cards should be represented as a string such as "Ace of Hearts"
+            // There are four suits: "Clubs", "Diamonds", "Hearts", and "Spades".
+            var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            //length of suits
+            var suitsLength = suits.Count;
+            // There are 13 ranks: "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", and "King".
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            //length of ranks
+            var ranksLength = ranks.Count;
+            //these nested foreach loops is like having many loops but with a multiplication effect! :-0 DRY \0/
+            //Make a loop that goes through the list of ranks
+            foreach (var suit in suits)
+            {
+                //Make a loop that goes through the list of ranks
+                // Console.WriteLine($"SuitsLength: {suitsLength}.");
+                foreach (var rank in ranks)
+                {
+                    //displaying the cards each of the 52
+                    var cards = $"{rank} of {suit}";
+                    //printing the cards
+                    Console.WriteLine($"and it's a: {cards}");
+                    deck.Add(cards);
+                    //this makes the loop go as many times as there are elements on the rank
+                    // Console.WriteLine("\n");
+                }
+                // Console.Write($"Total of cards : {deck.Count} ");
+                // Console.WriteLine("Counting my suits and there are : " + deck.Count + " of them at this point.");
+            };
+            var allCardsOnTheDeck = deck.Count;
+            //How do we shuffle?
+            //for rightIndex from numberOfCards - 1 down to 1 do:
+            for (var rightIndex = allCardsOnTheDeck - 1; rightIndex >= 1; rightIndex--)
+            {
+                //leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. 
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                // Now swap the values at rightIndex and leftIndex by doing this:
+                var leftCard = deck[leftIndex];
+                var rightCard = deck[rightIndex];
+                deck[leftIndex] = rightCard;
+                deck[rightIndex] = leftCard;
+                // After the deck is shuffled, display the top two cards.
+                //Showing firstCard
+                var firstCard = deck[0];
+                //Showing SecondCard
+                var secondCard = deck[1];
+                Console.WriteLine($"The first card is: {firstCard}, the second card is: {secondCard}");
+                //and then swaps that card with a randomly selected element that comes before it.
+            };
+            Console.WriteLine("End of Shuffle");
         }
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,121 @@
 # AllCardsOnDeckCS
+
+<!-- PEDAC (Problem, Example, Data (Data Structure), Algorithm and Code.)
+
+Problem Statement
+******************
+Explorer Mode Parts:
+Once the program starts, you should create a new deck.
+deckOfCards
+After deck creation, you should shuffle the deck.
+*****How do I shuffle the cards?****
+After the deck is shuffled, display the top two cards.
+firstCard
+secondCard
+
+Requirements:
+Your deck should contain 52 unique cards.
+All cards should be represented as a string such as "Ace of Hearts"
+There are four suits: "Clubs", "Diamonds", "Hearts", and "Spades".
+There are 13 ranks: "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", and "King".
+
+Create a deck of 52 cards
+Store cards in a variable as deck
+Represent the cards as a list of cards in the form of a list of strings per cards.
+
+
+
+Example Data:
+******************
+Create a deck of The deck of cards should contain 52 cards.
+All cards should be represented as a string like this:
+"Ace of Clubs",
+"2 of Clubs",
+"3 of Clubs",
+"4 of Clubs",
+"5 of Clubs",
+"6 of Clubs",
+"7 of Clubs",
+"8 of Clubs",
+"9 of Clubs",
+"10 of Clubs",
+"Jack of Clubs",
+"Queen of Clubs,"
+"King of Clubs",
+"Ace of Diamonds",
+"2 of Diamonds",
+"3 of Diamonds",
+"4 of Diamonds",
+"5 of Diamonds",
+"6 of Diamonds",
+"7 of Diamonds",
+"8 of Diamonds",
+"9 of Diamonds",
+"10 of Diamonds",
+"Jack of Diamonds",
+"Queen of Diamonds",
+"King of Diamonds",
+"Ace of Hearts",
+"2 of Hearts",
+"3 of Hearts",
+"4 of Hearts",
+"5 of Hearts",
+"6 of Hearts",
+"7 of Hearts",
+"8 of Hearts",
+"9 of Hearts",
+"10 of Hearts",
+"Jack of Hearts",
+"Queen of Hearts",
+"King of Hearts",
+"Ace of Spades",
+"2 of Spades",
+"3 of Spades",
+"4 of Spades",
+"5 of Spades",
+"6 of Spades",
+"7 of Spades",
+"8 of Spades",
+"9 of Spades",
+"10 of Spades",
+"Jack of Spades",
+"Queen of Spades",
+"King of Spades"
+
+Data Structure
+******************
+'strings'
+'list'
+'loops'
+There are four suits: "Clubs", "Diamonds", "Hearts", and "Spades".
+There are 13 ranks: "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", and "King".
+
+Algorithm
+******************
+
+Algorithm A
+-Make a empty string list and name it 'deck' and initialize them.
+-Make two lists, one for the 'ranks' and initialize them.
+-Make another one for the 'suits' and initialize them.
+-Then, we will loop through both lists to add all of our list elements to a deck list. Once we loop, we would have built a deck of cards.
+-Initialize the list of strings with 52 cards like in the example data to start and make sure we are getting the desired results.
+For example an empty list.
+-Shuffle them with Fisher Yates :
+
+  numberOfCards = length of our deck
+
+  for rightIndex from numberOfCards - 1 down to 1 do:
+    leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
+
+    Now swap the values at rightIndex and leftIndex by doing this:
+      leftCard = the value from deck[leftIndex]
+      rightCard = the value from deck[rightIndex]
+      deck[rightIndex] = leftCard
+      deck[leftIndex] = rightCard
+  *********end of Fisher Yates Algorithm
+
+-first card = deck[0]
+-second card = deck[1]
+-print first and second card
+
+-->
